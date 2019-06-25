8f63ef879f9879d560456fa5eb1a5bef6824ee95
WhatKindOf <50726981+WhatKindOf@users.noreply.github.com>
2019-06-25 20:41:06 +0900

    Updates

---
+++ precache-manifest.eff3cc49d3ef408bdf10b68ac26982c1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee9691004117fdd612db8941871005c",
+    "url": "/weather_forecast/index.html"
+  },
+  {
+    "revision": "0316fcacebd11e3575f1",
+    "url": "/weather_forecast/static/css/main.828a66e0.chunk.css"
+  },
+  {
+    "revision": "58d1d85aeac58fb0f191",
+    "url": "/weather_forecast/static/js/2.ecb748c5.chunk.js"
+  },
+  {
+    "revision": "0316fcacebd11e3575f1",
+    "url": "/weather_forecast/static/js/main.4cefa7e9.chunk.js"
+  },
+  {
+    "revision": "f91ada4785e359735968",
+    "url": "/weather_forecast/static/js/runtime~main.0ad8bc9d.js"
+  },
+  {
+    "revision": "fa6647d052b7d00190a405a64baa01d7",
+    "url": "/weather_forecast/static/media/Clear.fa6647d0.png"
+  },
+  {
+    "revision": "0e745d4c1d325685e7832366718af447",
+    "url": "/weather_forecast/static/media/Clouds.0e745d4c.png"
+  },
+  {
+    "revision": "29bb998c1b955e7a9e1431afbee0d3be",
+    "url": "/weather_forecast/static/media/Drizzle.29bb998c.png"
+  },
+  {
+    "revision": "722e3bfbec33c2f3dc1097e3f3cdc981",
+    "url": "/weather_forecast/static/media/Haze.722e3bfb.png"
+  },
+  {
+    "revision": "722e3bfbec33c2f3dc1097e3f3cdc981",
+    "url": "/weather_forecast/static/media/Mist.722e3bfb.png"
+  },
+  {
+    "revision": "57e92b24653830371af633d931c1f575",
+    "url": "/weather_forecast/static/media/Rain.57e92b24.png"
+  },
+  {
+    "revision": "ae7f45734b6fde1279e225f940812455",
+    "url": "/weather_forecast/static/media/Snow.ae7f4573.png"
+  },
+  {
+    "revision": "e1831bb2dfd41ebe55b78a7aceacdc87",
+    "url": "/weather_forecast/static/media/Thunderstorm.e1831bb2.png"
+  }
+]);
